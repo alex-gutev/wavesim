e6fbd07000c0301f6f52fa9cfb27fc14b8a44f43
Alexander Gutev <alex.gutev@gmail.com>
2026-01-25 18:23:27 +0100

    Add form for entering circular pulse wave source.

---
+++ lib/components/controls/select.dart
@@ -0,0 +1,68 @@
+import 'package:jaspr/jaspr.dart';
+import 'package:live_cells_core/live_cells_core.dart';
+import 'package:live_cells_jaspr/live_cells_jaspr.dart';
+
+/// Signature of [Select] option component builder callback.
+///
+/// Functions of this type are passed the build [context] and the [option]
+/// for which to build the component.
+typedef OptionBuilder<T> =
+  Component Function(BuildContext context, T option);
+
+/// A control for selecting an option out of a dropdown list of [options].
+///
+/// [options] is the list of options, from which the user may select. These
+/// are rendered as a drop down list. [builder] is called to build the
+/// user facing component for each option in [options].
+///
+/// The cell [selected] holds the option selected by the user. Changing the
+/// value of this cell, changes the selected option. Similarly, when an option
+/// is selected by the user the value of this cell is updated to reflect the
+/// change.
+///
+/// **NOTE**: [T] should provide sensible implementations of the  [==] and
+/// [hashCode] methods.
+class Select<T extends Object> extends CellComponent {
+  /// List of options from which the user may select
+  final List<T> options;
+
+  /// Cell holding the selected option
+  final MutableCell<T> selected;
+
+  /// Option component callback function
+  final OptionBuilder<T> builder;
+
+  /// Optional title to display above the control
+  final String? title;
+
+  const Select({
+    super.key,
+    required this.options,
+    required this.selected,
+    required this.builder,
+    this.title
+  });
+
+  @override
+  Component build(BuildContext context) => label([
+    div(classes: 'field', [
+      if (title != null)
+        span([
+          text(title!)
+        ]),
+      select(
+          onChange: (selection) {
+            selected.value = options[int.parse(selection.first)];
+          },
+
+          List.generate(options.length, (i) => option(
+              selected: options[i] == selected,
+              value: i.toString(),
+              [
+                builder(context, options[i])
+              ]
+          ))
+      )
+    ])
+  ]);
+}
--- lib/pages/simulation/wave_source_control.dart
+++ lib/pages/simulation/wave_source_control.dart
@@ -2,13 +2,25 @@ import 'package:jaspr/jaspr.dart';
 import 'package:live_cells_core/live_cells_core.dart';
 import 'package:live_cells_jaspr/live_cells_jaspr.dart';
 
+import '../../components/controls/num_field.dart';
+import '../../components/controls/select.dart';
 import '../../components/controls/int_vector_field.dart';
 import '../../components/controls/num_vector_field.dart';
 import '../../components/dialog/index.dart';
 import '../../components/layout/index.dart';
 import '../../simulator/wave_source.dart';
 import '../../sources/point_source.dart';
+import '../../sources/circle_pulse.dart';
 import '../../util/types.dart';
+
+/// Identifies the type of wave source
+enum WaveType {
+  /// A pulse at a single point
+  pointPulse,
+
+  /// A pulse at the perimeter of a circle
+  circlePulse
+}
 
 /// A button for adding a wave source to the simulation
 class WaveSourceControl extends CellComponent {
@@ -65,6 +77,98 @@ class _WaveSourceDialog extends CellComponent {
 
   @override
   Component build(BuildContext context) {
+    final type = MutableCell(WaveType.pointPulse);
+    final source = MetaCell<WaveSource>();
+
+    return Dialog(
+        open: open,
+        [
+          form(method: FormMethod.dialog, [
+            Column([
+              h1([
+                text('Add Wave')
+              ]),
+              Select(
+                options: WaveType.values,
+                selected: type,
+                builder: (context, type) => switch (type) {
+                  WaveType.pointPulse => text('Point Pulse'),
+                  WaveType.circlePulse => text('Circular Pulse'),
+                }
+              ),
+              _WaveSourceParameters(
+                  source: source,
+                  size: size,
+                  type: type
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.end, [
+                button(
+                    type: ButtonType.button,
+                    onClick: () => MutableCell.batch(() {
+                      open.value = false;
+                    }),
+
+                    [text('Cancel')]
+                ),
+                button(
+                    autofocus: true,
+                    onClick: () => onClose(source.value),
+                    [text('Add')]
+                )
+              ])
+            ])
+          ])
+        ]
+    );
+  }
+}
+
+/// Provides fields for entering the parameters of a wave source of a given [type].
+class _WaveSourceParameters extends CellComponent {
+  /// Meta cell injected with a cell that constructs the wave source of the given [type].
+  final MetaCell<WaveSource> source;
+
+  /// The size of the grid
+  final ValueCell<int> size;
+
+  /// The type of wave source to create
+  final ValueCell<WaveType> type;
+
+  const _WaveSourceParameters({
+    required this.source,
+    required this.size,
+    required this.type
+  });
+
+  @override
+  Component build(BuildContext context) => switch (type()) {
+    WaveType.pointPulse => _PointPulseForm(
+        source: source,
+        size: size
+    ),
+
+    WaveType.circlePulse => _CirclePulseForm(
+        source: source,
+        size: size
+    ),
+  };
+}
+
+/// Form for entering the parameters of a point pulse wave source
+class _PointPulseForm extends CellComponent {
+  /// Meta cell injected with a cell that constructs the wave source.
+  final MetaCell<WaveSource> source;
+
+  /// The size of the grid
+  final ValueCell<int> size;
+
+  const _PointPulseForm({
+    required this.source,
+    required this.size
+  });
+
+  @override
+  Component build(BuildContext context) {
     final position = MutableCell(
         VectorI(
             x: 0,
@@ -79,54 +183,88 @@ class _WaveSourceDialog extends CellComponent {
         )
     );
 
-    return Dialog(
-        open: open,
-        [
-          form(method: FormMethod.dialog, [
-            Column([
-              h1([
-                text('Add Wave')
-              ]),
-              IntVectorField(
-                  title: 'Position',
-                  value: position,
-                  min: VectorI(
-                      x: -size(),
-                      y: -size()
-                  ),
-                  max: VectorI(
-                      x: size(),
-                      y: size()
-                  )
-              ),
-              NumVectorField(
-                title: 'Amplitude',
-                value: amplitude,
-              ),
-              Row(mainAxisAlignment: MainAxisAlignment.end, [
-                button(
-                    type: ButtonType.button,
-                    onClick: () => MutableCell.batch(() {
-                      open.value = false;
-                    }),
-
-                    [text('Cancel')]
-                ),
-                button(
-                    autofocus: true,
-                    onClick: () => onClose(
-                        PointPulse(
-                            position: position.value,
-                            amplitude: amplitude.value
-                        )
-                    ),
-
-                    [text('Add')]
-                )
-              ])
-            ])
-          ])
-        ]
+    source.inject(
+      ValueCell.computed(() => PointPulse(
+          position: position(),
+          amplitude: amplitude()
+      ))
     );
+
+    return fragment([
+      IntVectorField(
+          title: 'Position',
+          value: position,
+          min: VectorI(
+              x: -size(),
+              y: -size()
+          ),
+          max: VectorI(
+              x: size(),
+              y: size()
+          )
+      ),
+      NumVectorField(
+        title: 'Amplitude',
+        value: amplitude,
+      )
+    ]);
+  }
+}
+
+/// Form for entering the parameters of a circular pulse wave source.
+class _CirclePulseForm extends CellComponent {
+  /// Meta cell injected with a cell that constructs the wave source.
+  final MetaCell<WaveSource> source;
+
+  /// The size of the grid
+  final ValueCell<int> size;
+
+  const _CirclePulseForm({
+    required this.source,
+    required this.size
+  });
+
+  @override
+  Component build(BuildContext context) {
+    final center = MutableCell(
+        VectorI(
+            x: 0,
+            y: 0
+        )
+    );
+
+    final radius = MutableCell(5.0);
+    final amplitude = MutableCell(-1.0);
+
+    source.inject(
+        ValueCell.computed(() => CirclePulse(
+            center: center(),
+            radius: radius(),
+            amplitude: amplitude()
+        ))
+    );
+
+    return fragment([
+      IntVectorField(
+          title: 'Centre',
+          value: center,
+          min: VectorI(
+              x: -size(),
+              y: -size()
+          ),
+          max: VectorI(
+              x: size(),
+              y: size()
+          )
+      ),
+      NumField(
+        title: 'Radius',
+        value: radius
+      ),
+      NumField(
+        title: 'Amplitude',
+        value: amplitude,
+      )
+    ]);
   }
 }
