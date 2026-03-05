2010d90fc91bced89b767b74f2ed37818097b77c
Alexander Gutev <alex.gutev@gmail.com>
2026-03-05 17:59:26 +0100

    Refactor frequency and pulse control into separate class.

---
+++ lib/sources/alternating_source.dart
@@ -0,0 +1,46 @@
+import 'dart:math';
+
+import 'package:jaspr/jaspr.dart';
+import 'package:live_cells_core/live_cells_core.dart';
+import 'package:wavesim/simulator/wave_source.dart';
+
+import '../simulator/wavesim_engine_2d.dart';
+
+part 'alternating_source.g.dart';
+
+/// Base class for a source with an alternating amplitude.
+///
+/// The amplitude alternates with the given [frequency]. This class can also
+/// be used to create a source that is only kept for a given number of timesteps,
+/// which is given by [maxSteps].
+@CellExtension()
+abstract class AlternatingSource implements WaveSource {
+  /// Frequency of the wave
+  final double frequency;
+
+  /// The maximum number of time steps for which the source should be kept.
+  ///
+  /// If null the source is kept indefinitely. If null, the source is only kept
+  /// for this number of time steps.
+  final int? maxSteps;
+
+  /// The scale by which to multiply the amplitude at the current time.
+  double get scale => cos(pi * frequency * _time / 10);
+
+  AlternatingSource({
+    required this.frequency,
+    required this.maxSteps
+  });
+
+  @override
+  @mustCallSuper
+  bool update(WavesimEngine2D engine) {
+    _time++;
+
+    return maxSteps != null
+        ? _time < maxSteps!
+        : true;
+  }
+
+  var _time = 0;
+}
--- lib/sources/point_source.dart
+++ lib/sources/point_source.dart
@@ -1,45 +1,33 @@
-import 'dart:math';
-
 import 'package:live_cells_core/live_cells_core.dart';
 
+import 'alternating_source.dart';
 import '../simulator/wavesim_engine_2d.dart';
-import '../simulator/wave_source.dart';
 import '../util/types.dart';
 
 part 'point_source.g.dart';
 
 /// A wave source emanating from a single point.
 ///
-/// This source creates a wave emanating from a give [position], within the
-/// simulation grid, with a given [amplitude]. The pulse is only created for
-/// one time step after which the source is removed.
+/// This source creates a wave emanating from a give [position] with a given
+/// [amplitude].
 @CellExtension(mutable: true)
-class PointSource implements WaveSource {
+class PointSource extends AlternatingSource {
   /// Position at which to create the pulse
   final Vector<int> position;
 
   /// Amplitude of the pulse
   final Vector<double> amplitude;
 
-  /// Frequency of the wave
-  final double frequency;
-
-  /// The maximum number of time steps for which the source should be kept.
-  ///
-  /// If null the source is kept indefinitely. If null, the source is only kept
-  /// for this number of time steps.
-  final int? maxSteps;
-
   PointSource({
     required this.position,
     required this.amplitude,
-    this.maxSteps,
-    this.frequency = 0
+    super.maxSteps,
+    super.frequency = 0
   });
 
   @override
   bool update(WavesimEngine2D engine) {
-    final f = cos(pi * frequency * _time / 10);
+    final f = scale;
 
     engine.displace(
         x: position.x,
@@ -48,12 +36,6 @@ class PointSource implements WaveSource {
         dy: f * amplitude.y
     );
 
-    _time++;
-
-    return maxSteps != null
-        ? _time < maxSteps!
-        : true;
+    return super.update(engine);
   }
-
-  var _time = 0;
 }
